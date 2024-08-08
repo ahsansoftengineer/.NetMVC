735ec9115c38576523d452218140046bc9fbd08c
Muhammad Ahsan Saifi <ahsan.saifi@starbazaar.pk>
2024-08-08 14:35:47 +0500

    REPO WORKING

--- Lagoon.App/Common/Interface/IRepoGeneric.cs
+++ Lagoon.App/Common/Interface/IRepoGeneric.cs
@@ -4,10 +4,10 @@ namespace Lagoon.App.Common.Interface;
 public interface IRepoGeneric<T>
 where T : class
 {
-  void Save();
+  // void Save();
   void Add(T entity);
-  void Update(T entity);
   void Remove(T entity);
+  // void Update(T entity);
   IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? include = null);
   T? Get(Expression<Func<T, bool>> filter, string? include = null);
 }
--- Lagoon.App/Common/Interface/IRepoVilla.cs
+++ Lagoon.App/Common/Interface/IRepoVilla.cs
@@ -4,5 +4,8 @@ using Lagoon.Domain.Entity;
 namespace Lagoon.App.Common;
 public interface IRepoVilla : IRepoGeneric<Villa>
 {
+  void Update(Villa entity);
+  void Save();
+
   
 }
